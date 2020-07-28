33b6864725bcba527e7c2049727b1d966634062f
Minematas <minematas@gmail.com>
2020-07-28 17:58:42 +0300

    Added setup

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setuptools
+
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+with open('requirements.txt') as f:
+    requirements = f.readlines()
+
+setuptools.setup(
+    name='pyprerender',
+    version='0.0.1',
+    author='Matas Minelga',
+    author_email='minematas@gmail.com',
+    description='A python prerenderer',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/thingiesmm/pyprerender',
+    python_requires='>=3.7',
+    install_requires=requirements,
+    package_dir={"": "src"},
+    packages=['pyprerender'],
+    zip_safe=False,
+    license='MIT',
+    classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+)
